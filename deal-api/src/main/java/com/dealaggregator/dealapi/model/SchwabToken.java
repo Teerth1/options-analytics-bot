bbd4fd146da286f18cb7b7ef93c4589ba7d8b4e7
Teerth1 <137332257+Teerth1@users.noreply.github.com>
2026-01-20 18:29:02 -0500

    Fix: Remove conflicting Lombok annotations and unused imports in SchwabToken

--- deal-api/src/main/java/com/dealaggregator/dealapi/model/SchwabToken.java
+++ deal-api/src/main/java/com/dealaggregator/dealapi/model/SchwabToken.java
@@ -3,15 +3,9 @@ package com.dealaggregator.dealapi.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "schwab_tokens")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class SchwabToken {
     @Id
     private String id; // We'll use a constant ID like "default" to keep it simple (singleton row)
